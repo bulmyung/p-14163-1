ac64a5298558fd5aebd79337c5c3a5c11b71f6e8
bulmyung <bulmyun9@gmail.com>
2026-05-20 16:33:56 +0900

    260520-34

--- IdeaProjects/p-14163-2/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ IdeaProjects/p-14163-2/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -2,42 +2,21 @@ package com.mysite.sbb.question;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
 public class QuestionController {
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
     @GetMapping("/question/list")
-    @ResponseBody
-    public String list() {
-        List<Question> questions = questionRepository.findAll();
+    public String list(Model model) {
+        List<Question> questionList = questionService.getList();
+        model.addAttribute("questionList",questionList);
 
-        String questionLiHtml = questions
-                .stream()
-                .map(q -> "<li>%d / $s</li>".formatted(q.getId(),q.getSubject()))
-                .collect(Collectors.joining("\n\t\t"));
-
-        return """
-                <!DOCTYPE html>
-                <html lang="ko">
-                <head>
-                    <meta charset="UTF-8">
-                    <title>질문 목록</title>
-                </head>
-                <body>
-                    <h1>질문 목록<h1>
-                    
-                    <ul>
-                        %s
-                    </ul>
-                </body>
-                </html>
-                """.formatted(questionLiHtml);
+        return "question_list";
     }
 }
