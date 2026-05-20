26583493ef243c77ec91477c5645be93afb96811
bulmyung <bulmyun9@gmail.com>
2026-05-20 15:33:09 +0900

    260520-16

--- IdeaProjects/p-14163-2/src/test/java/com/mysite/sbb/QuestionRepositoryTest.java
+++ IdeaProjects/p-14163-2/src/test/java/com/mysite/sbb/QuestionRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -18,23 +19,31 @@ class QuestionRepositoryTest {
     @Test
     @DisplayName("findAll")
     void t1() {
-        List<Question> all = questiongRepository.findAll();
-        assertEquals(2, all.size());
+        List<Question> questions = questiongRepository.findAll();
+        assertThat(questions).hasSize(2);
 
-        Question q = all.get(0);
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
+        Question question = questions.get(0);
+        assertThat(question.getSubject()).isEqualTo("sbb가 무엇인가요?");
     }
 
     @Test
     @DisplayName("findById")
     void t2() {
+        Question question = questiongRepository.findById(1).get();
+        assertThat(question.getSubject()).isEqualTo("sbb가 무엇인가요?");
+    }
 
-        Optional<Question> oq = questiongRepository.findById(1);
+    @Test
+    @DisplayName("findBySubject")
+    void t3() {
+        Question question = questiongRepository.findBySubject("sbb가 무엇인가요?").get();
+        assertThat(question.getId()).isEqualTo(1);
+    }
 
-        if (oq.isPresent()) {
-            Question q = oq.get();
-            assertEquals("sbb가 무엇인가요?", q.getSubject());
-        }
-
+    @Test
+    @DisplayName("findBySubjectAndContent")
+    void t4() {
+        Question question = questiongRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.").get();
+        assertThat(question.getId()).isEqualTo(1);
     }
 }
