97dc21687f27fe802b790caaaa1b254601ac5278
bulmyung <bulmyun9@gmail.com>
2026-05-25 16:46:02 +0900

    7

---
+++ IdeaProjects/p-14163-3/src/main/java/com/mysite/sbb/Question.java
@@ -0,0 +1,24 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answers;
+}
